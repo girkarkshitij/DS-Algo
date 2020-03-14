e8c368a5f32a1e82b3f7cd3d5ccf4d1ef3230142
Kshitij Girkar <43858028+girkarkshitij@users.noreply.github.com>
2020-03-14 20:22:35 +0530

    Create InsertionSort.java

---
+++ Sorting Algorithms/InsertionSort.java
@@ -0,0 +1,36 @@
+/**
+ * Time Complexity--> Best: O(N), Average: O(N^2), Worst: O(N^2)
+ * Space Complexity--> O(1)
+ */
+
+import java.util.Scanner;
+
+class Test{
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int n=scanner.nextInt();
+        int[] array=new int[n];
+        for (int i = 0; i <n ; i++) {
+            array[i]=scanner.nextInt();
+        }
+        array= insertionSort(array);
+        for (int a :
+                array) {
+            System.out.println(a);
+        }
+    }
+
+    private static  int[] insertionSort(int[] array) {
+        int n=array.length;
+        for (int i = 1; i <n ; i++) {
+            int value=array[i];
+            int empty=i;
+            while (empty>0 && array[empty-1]>value){
+                array[empty]=array[empty-1];
+                empty--;
+            }
+            array[empty]=value;
+        }
+        return array;
+    }
+}
