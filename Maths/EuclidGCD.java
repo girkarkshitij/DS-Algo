f47b2f8cda691d5141e398e6584247f229b77a41
Kshitij Girkar <43858028+girkarkshitij@users.noreply.github.com>
2020-03-31 12:24:03 +0530

    Create EuclidGCD.java

---
+++ Maths/EuclidGCD.java
@@ -0,0 +1,20 @@
+//Euclidean algorithm to find GCD of two numbers
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main{
+	public static void main (String[] args){
+		System.out.print(gcd(8,6));  //2
+		System.out.print(gcd(1,6));  //1
+		System.out.print(gcd(0,8));  //8
+	}
+	
+	public static int gcd(int a,int b){
+		if(b == 0)
+			return a;
+		else
+			return gcd(b, a%b);
+	}
+}
