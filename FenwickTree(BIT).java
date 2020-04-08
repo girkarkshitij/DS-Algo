41e55e688073b5977144ff2cb557c49193a3521d
Kshitij Girkar <43858028+girkarkshitij@users.noreply.github.com>
2020-04-08 19:16:26 +0530

    Now compatible with long data type

--- FenwickTree(BIT).java
+++ FenwickTree(BIT).java
@@ -1,19 +1,19 @@
 class BinaryIndexedTree {
 
-    public int[] array;
-    public int[] BIT;
+    public long[] array;
+    public long[] BIT;
     public int n;
 
-    public BinaryIndexedTree(int[] inputArray) {
+    public BinaryIndexedTree(long[] inputArray) {
         this.array = inputArray;
 
         n = inputArray.length;
-        BIT = new int[n + 1];
+        BIT = new long[n + 1];
         for (int i = 0; i < n; i++)
             init(i, inputArray[i]);
     }
 
-    public void init(int i, int val) {
+    public void init(int i, long val) {
         i++;
         while (i <= n) {
             BIT[i] += val;
@@ -21,14 +21,14 @@ class BinaryIndexedTree {
         }
     }
 
-    void update(int index, int val) {
-        int diff = val - array[index];
+    void update(int index, long val) {
+        long diff = val - array[index];
         array[index] = val;
         init(index, diff);
     }
 
-    public int getSum(int index) {
-        int sum = 0;
+    public long getSum(int index) {
+        long sum = 0;
         index++;
         while (index > 0) {
             sum += BIT[index];
@@ -37,14 +37,14 @@ class BinaryIndexedTree {
         return sum;
     }
 
-    public int sumRange(int start, int end) {
+    public long sumRange(int start, int end) {
         return getSum(end) - getSum(start - 1);
     }
 }
 
-public class Main2 {
+public class Test {
     public static void main(String[] xps){
-        int[] array={1,2,3,4,5,6,7,8,9,10};
+        long[] array = {1,2,3,4,5,6,7,8,9,10};
         BinaryIndexedTree binaryIndexedTree =new BinaryIndexedTree(array);
         System.out.println(binaryIndexedTree.sumRange(0,9));
 
@@ -54,5 +54,11 @@ public class Main2 {
         System.out.println(binaryIndexedTree.sumRange(0,9));
 
         //Output : 65
+
+        long[] array2 = {100000000000L, 230000000000L, 20002403020022L, 4054939292922L};
+        BinaryIndexedTree binaryIndexedTree1 = new BinaryIndexedTree(array2);
+        System.out.println(binaryIndexedTree1.sumRange(0,1));
+
+        //Output : 330000000000
     }
 }
