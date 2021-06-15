2bd6944f10b9e989e043daded6bf25d82e0b4f73
Kshitij Girkar <43858028+girkarkshitij@users.noreply.github.com>
2021-06-15 12:18:32 +0530

    Update MergeSort.java

--- Sorting Algorithms/MergeSort.java
+++ Sorting Algorithms/MergeSort.java
@@ -5,53 +5,57 @@
  * Worst-case space complexity-->	О(n) total with O(n) auxiliary, O(1) auxiliary with linked lists[1]
  */
 
-class Merge{
-    private static int[] aux;
 
-    public void sort(int[] a){
-        aux=new int[a.length];
-        sort(a,0,a.length-1);
-    }
+import java.util.Arrays;
 
-    public void sort(int[] a,int lo,int hi){
-        if (hi<=lo)
-            return;
-        int mid=lo+(hi-lo)/2;
-        sort(a,lo,mid);
-        sort(a,mid+1,hi);
-        merge(a,lo,mid,hi);
-    }
-
-    public void merge(int[] a,int lo,int mid,int hi){
-        int i=lo;
-        int j=mid+1;
-        int[] aux=new int[a.length];
-
-        for (int k = lo; k <=hi  ; k++) {
-            aux[k]=a[k];
-        }
-
-        for (int k = lo; k <=hi ; k++) {
-            if (i>mid)
-                a[k]=aux[j++];
-            else if (j>hi)
-                a[k]=aux[i++];
-            else if (aux[j]<aux[i])
-                a[k]=aux[j++];
-            else
-                a[k]=aux[i++];
-        }
-    }
-
-}
 class Main{
     public static void main(String[] args) {
-        int[] array={99,4,45,45,45,45,48,78,5,2,36};
-        Merge m1=new Merge();
-        m1.sort(array);
-        for (int a :
-                array) {
-            System.out.print(a+" ");
+        int[] a = {4, 1, 3, 7, 9};
+        mergeSort(a, 0, a.length-1);
+        System.out.println(Arrays.toString(a));
+    }
+
+    public static void merge(int[] a, int l, int m, int r){
+        int[] left = Arrays.copyOfRange(a, l, m+1);
+        int[] right = Arrays.copyOfRange(a, m+1, r+1);
+
+        int i = 0, j = 0, k = l;
+
+        while(i < left.length && j < right.length) {
+            if(left[i] < right[j]) {
+                a[k] = left[i];
+                k++;
+                i++;
+            }else{
+                a[k] = right[j];
+                k++;
+                j++;
+            }
+        }
+
+        if(i < left.length){
+            while(i < left.length){
+                a[k] = left[i];
+                i++;
+                k++;
+            }
+        }
+        if(j < right.length){
+            while(j < right.length){
+                a[k] = right[j];
+                j++;
+                k++;
+            }
+        }
+    }
+
+    public static void mergeSort(int[] a, int l, int r){
+        if(l < r) {
+            int m = l + (r - l) / 2;
+            mergeSort(a, l, m);
+            mergeSort(a, m + 1, r);
+            merge(a, l, m, r);
         }
     }
 }
+
